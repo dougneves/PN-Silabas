808cab57b59133d42d4baad7d7c881128e33689e
Douglas Neves <dougneves@gmail.com>
2016-11-18 01:39:42 -0200

    better tts funcionality

--- main.js
+++ main.js
@@ -14,44 +14,80 @@ window.onload = () => {
     })
   }
 
-  //fala texto
   const textToSpeech = (text) => {
+    return new Promise((resolve,reject) => {
+      tts(text);
+      setTimeout(() => {resolve()},text.length*200);
+    });
+  }
+
+  //fala texto
+  const tts = (text) => {
     selectVoice();
-    const utterThis = new SpeechSynthesisUtterance(text+".");
+    const utterThis = new SpeechSynthesisUtterance(text);
     utterThis.voice = voice;
-    console.log("lendo "+text);
     synth.speak(utterThis);
   }
 
   //desafios
   const vogais = ["a","e","i","o","u"];
   const consoantes = ["b","c","d","f","g","j","l","m","n","p","r","s","t","w","x","z"];
+  const silabas = [
+    {screen: "BA", tts: "bá"},
+    {screen: "BE", tts: "bé"},
+    {screen: "BI", tts: "bí"},
+    {screen: "BO", tts: "bó"},
+    {screen: "BU", tts: "bú"},
+    {screen: "CA", tts: "cá"},
+    {screen: "CO", tts: "có"},
+    {screen: "CU", tts: "cú"},
+    {screen: "DA", tts: "dá"},
+    {screen: "DE", tts: "dé"},
+    {screen: "DI", tts: "dí"},
+    {screen: "DO", tts: "dó"},
+    {screen: "DU", tts: "dú"},
+  ]
+  const elogios = [
+    "Parabéns",
+    "Muito bem",
+    "Isso Mesmo",
+    "Você acertou",
+    "Está certo",
+    "Brilhante",
+    "Correto",
+    "Gostei"
+  ]
 
   //cria desafios
   let desafio;
 
   const verifyAnwser = (event) => {
+    enabled(false);
     const src = event.srcElement;
-    console.log(src.value);
-    if(src.value === desafio) {
-      textToSpeech(src.value+". Muito bem Alícia! Você acertou!")
-      newChallange();
+    if(src.value === desafio.screen) {
+      const frases = _.shuffle(elogios);
+      textToSpeech("É "+desafio.tts+". "+frases[1]+" Alícia! "+frases[2]+"!").then(()=>{
+        newChallange();
+        enabled(true);
+      });
     }
     else {
-      textToSpeech(src.value+". Não.")
+      textToSpeech(src.value+". Não.").then(()=>{
+        enabled(true);
+      })
       src.disabled = true;
     }
   }
 
   const newChallange = () => {
-    desafio = _.sample(consoantes)+_.sample(vogais);
-    console.log("novo desafio = " +desafio);
-    textToSpeech(desafio)
+    const next = _.shuffle(silabas);
+    desafio = next[0];
+    textToSpeech(desafio.tts).then()
 
     const botoes = _.shuffle([
-      desafio,
-      _.sample(consoantes)+_.sample(vogais),
-      _.sample(consoantes)+_.sample(vogais)
+      next[0].screen,
+      next[1].screen,
+      next[2].screen
     ]);
 
     const div = document.createElement("div");
@@ -67,10 +103,15 @@ window.onload = () => {
 
   }
 
+  const enabled = (e) => {
+    let s = "none";
+    if(!e) s = "block";
+    document.getElementById('modalDisable').style.display = s;
+  }
+
   newChallange();
 
   setInterval(() => {
-    console.log("falando: " + desafio);
-    textToSpeech(desafio)
+    textToSpeech(desafio.tts).then()
   },6000)
 }
